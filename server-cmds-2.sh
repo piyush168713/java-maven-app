df49b0845f02799c09c9300a61073b191feb6807
piyush168713 <20BCS9107@cuchd.in>
2023-06-24 17:20:02 +0530

    pass-image-as-param-to-script

--- server-cmds-2.sh
+++ server-cmds-2.sh
@@ -1,7 +1,8 @@
 #!usr/bin/env bash
 
+export IMAGE = $1
 docker-compose -f docker-compose.yaml up --detach
 echo "success"
 export TEST=testvalue 
 
-# execute both cmds by running the shell script in jenkinsfile12
+# execute both cmds by running the shell script in jenkinsfile13
